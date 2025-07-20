3d9a0bb03532d62f9cc54dfc4c29e905079cd18d
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-07-20 03:14:22 -0500

    Render shop page elements correctly

--- src/components/shoppage/ProductCard.jsx
+++ src/components/shoppage/ProductCard.jsx
@@ -1,18 +1,66 @@
 import classes from "./shop.module.css";
+import { useState } from "react";
+
+function CustomInput({ productAmount, setProductAmount }) {
+  return (
+    <input
+      type="number"
+      value={productAmount}
+      name="productAmount"
+      onChange={(event) => setProductAmount(event.target.value)}
+    />
+  );
+}
 
 function ProductCard({ updateCartCount, productDetails }) {
+  const [productAmount, setProductAmount] = useState(0);
+
+  function updateInputValue(amount) {
+    setProductAmount((prev) => {
+      const newVal = prev + amount;
+      return newVal >= 0 ? newVal : 0;
+    });
+  }
+
+  function addToCart(amount) {
+    // do stuff
+    if (amount > 0) {
+      setProductAmount(0);
+    }
+  }
+
   return (
     <div className={`${classes.cardContainer}`}>
-      <img src="" alt="" />
-      <div>Product Name</div>
-      <div>
-        <button>-</button>
-        <label htmlFor="">
-          <input type="number" name="product-amount" id="" />
-        </label>
-        <button>+</button>
+      <img src={productDetails.image} className={`${classes.productImage}`} />
+
+      <div className={`${classes.information}`}>
+        <span className={`${classes.productTitle}`} title={productDetails.title}>
+          {productDetails.title}
+        </span>
+
+        <div className={`${classes.inputRow}`}>
+          <button
+            className={`${classes.button}`}
+            onClick={() => updateInputValue(-1)}
+          >
+            -
+          </button>
+
+          <CustomInput
+            productAmount={productAmount}
+            setProductAmount={setProductAmount}
+          />
+
+          <button
+            className={`${classes.button}`}
+            onClick={() => updateInputValue(1)}
+          >
+            +
+          </button>
+        </div>
+
+        <button className={`${classes.addCartButton}`} onClick={() => addToCart(productAmount)}>Add to Cart</button>
       </div>
-      <button>Add to Cart</button>
     </div>
   );
 }
